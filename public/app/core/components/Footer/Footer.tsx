9640a7df4e04cbb6092f5408ee298262a232ec51
Max Knodel <mknodel@elmatic.de>
2023-01-12 17:05:26 +0100

    Brand the navbar footer (#1)
    Signed-off-by: Max Knodel <m.knodel@elmatic.de>

--- public/app/core/components/Footer/Footer.tsx
+++ public/app/core/components/Footer/Footer.tsx
@@ -21,21 +21,21 @@ export let getFooterLinks = (): FooterLink[] => {
       id: 'documentation',
       text: t('nav.help/documentation', 'Documentation'),
       icon: 'document-info',
-      url: 'https://grafana.com/docs/grafana/latest/?utm_source=grafana_footer',
+      url: 'https://doc.elmatic-align.de',
     },
     {
       target: '_blank',
       id: 'support',
       text: t('nav.help/support', 'Support'),
       icon: 'question-circle',
-      url: 'https://grafana.com/products/enterprise/?utm_source=grafana_footer',
+      url: 'https://support.elmatic-align.de',
     },
     {
       target: '_blank',
       id: 'community',
-      text: t('nav.help/community', 'Community'),
+      text: t('nav.help/company', 'Elmatic'),
       icon: 'comments-alt',
-      url: 'https://community.grafana.com/?utm_source=grafana_footer',
+      url: 'https://www.elmatic.de',
     },
   ];
 };
@@ -75,16 +75,6 @@ export function getVersionLinks(hideEdition?: boolean): FooterLink[] {
     text: buildInfo.versionString,
     url: hasReleaseNotes ? `https://github.com/grafana/grafana/blob/main/CHANGELOG.md` : undefined,
   });
-
-  if (buildInfo.hasUpdate) {
-    links.push({
-      target: '_blank',
-      id: 'updateVersion',
-      text: `New version available!`,
-      icon: 'download-alt',
-      url: 'https://grafana.com/grafana/download?utm_source=grafana_footer',
-    });
-  }
 
   return links;
 }
