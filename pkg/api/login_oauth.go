fb861db1aa744c4602c736e3b4e735e7187321f7
dkelmatic <d.kauderer@elmatic.de>
2023-03-22 14:25:21 +0100

    added social login again

--- pkg/api/login_oauth.go
+++ pkg/api/login_oauth.go
@@ -286,6 +286,8 @@ func (hs *HTTPServer) buildExternalUserInfo(token *oauth2.Token, userInfo *socia
 		IsGrafanaAdmin: userInfo.IsGrafanaAdmin,
 	}
 
+	plog.Debug("CHECK:", "role", userInfo.Role, "hs.Cfg.OAuthSkipOrgRoleUpdateSync", hs.Cfg.OAuthSkipOrgRoleUpdateSync)
+	plog.Debug("cfg: ", hs.Cfg)
 	if userInfo.Role != "" && !hs.Cfg.OAuthSkipOrgRoleUpdateSync {
 		rt := userInfo.Role
 		if rt.IsValid() {
@@ -301,6 +303,20 @@ func (hs *HTTPServer) buildExternalUserInfo(token *oauth2.Token, userInfo *socia
 					"role", userInfo.Role, "orgId", orgID)
 			}
 			extUser.OrgRoles[orgID] = rt
+		}
+	} else {
+		// The structure of the group attribute is supposted to be as follows:
+		// <OrgId>:<RoleType> -> 1:admin 2:viewer [...]
+		for _, orgGroup := range userInfo.Groups {
+			// split the pairs apart
+			orgGroupSplit := strings.Fields(strings.ReplaceAll(orgGroup, ":", " "))
+			if len(orgGroupSplit) == 0 {
+				plog.Debug("Some went wrong reading groups/orgs from userInfo! GO on", "orgGroupSplit", orgGroupSplit)
+				continue
+			}
+			orgId, _ := strconv.Atoi(orgGroupSplit[0])
+			// Transfer gathered info into OrgRoles map
+			extUser.OrgRoles[int64(orgId)] = org.RoleType(orgGroupSplit[1])
 		}
 	}
 
