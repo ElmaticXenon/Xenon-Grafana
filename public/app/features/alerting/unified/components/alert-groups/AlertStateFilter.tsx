4c7ee4cd362072299bb81add8a22619068ae4783
jkaiser <j.kaiser@elmatic.de>
2024-11-28 16:39:46 +0100

    (#76) added translations for menu items: alerting/gropus

--- public/app/features/alerting/unified/components/alert-groups/AlertStateFilter.tsx
+++ public/app/features/alerting/unified/components/alert-groups/AlertStateFilter.tsx
@@ -4,6 +4,8 @@ import { SelectableValue } from '@grafana/data';
 import { RadioButtonGroup, Label } from '@grafana/ui';
 import { AlertState } from 'app/plugins/datasource/alertmanager/types';
 
+import { Trans, t } from '../../../../../core/internationalization';
+
 interface Props {
   stateFilter?: AlertState;
   onStateFilterChange: (value: AlertState) => void;
@@ -13,14 +15,22 @@ export const AlertStateFilter = ({ onStateFilterChange, stateFilter }: Props) =>
   const alertStateOptions: SelectableValue[] = Object.entries(AlertState)
     .sort(([labelA], [labelB]) => (labelA < labelB ? -1 : 1))
     .map(([label, state]) => ({
-      label,
+      label: createTransKey(label),
       value: state,
     }));
 
   return (
     <div>
-      <Label>State</Label>
+      <Label>
+        <Trans i18nKey="alert-groups.state">State</Trans>
+      </Label>
       <RadioButtonGroup options={alertStateOptions} value={stateFilter} onChange={onStateFilterChange} />
     </div>
   );
 };
+
+function createTransKey(str: string) {
+  let transKey = str.toLowerCase();
+  transKey = 'alert-groups.' + transKey;
+  return t(transKey, str);
+}
